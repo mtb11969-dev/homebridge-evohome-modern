85d2616ddef08775a170bec5f715f6864d0d796f
mtb11969-dev <mtb1.1969@icloud.com>
2026-08-06 21:10:50 +0200

    Update  client.js

--- src/api/ client.js
+++ src/api/ client.js
@@ -12,16 +12,39 @@ class EvohomeClient {
         this.token = token;
     }
 
+    async request(method, path, body = null) {
+
+        const options = {
+            method,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        if (this.token) {
+            options.headers.Authorization = this.token;
+        }
+
+        if (body) {
+            options.body = JSON.stringify(body);
+        }
+
+        this.log.debug(`${method} ${this.baseUrl}${path}`);
+
+        // Ide kerül majd a fetch() hívás.
+        return null;
+    }
+
     async get(path) {
-        this.log.debug(`GET ${path}`);
+        return this.request('GET', path);
     }
 
     async post(path, body) {
-        this.log.debug(`POST ${path}`);
+        return this.request('POST', path, body);
     }
 
     async put(path, body) {
-        this.log.debug(`PUT ${path}`);
+        return this.request('PUT', path, body);
     }
 
 }

e3805f599affd8764fd310cfcb5cdbedc5f501aa
mtb11969-dev <mtb1.1969@icloud.com>
2026-08-06 20:17:09 +0200

    Update platform.js

--- src/platform.js
+++ src/platform.js
@@ -1,1 +1,19 @@
+'use strict';
 
+class EvohomePlatform {
+
+  constructor(log, config, api) {
+
+    this.log = log;
+    this.config = config || {};
+    this.api = api;
+
+    this.username = this.config.username;
+    this.password = this.config.password;
+
+    this.log.info('Homebridge Evohome Modern starting...');
+  }
+
+}
+
+module.exports = EvohomePlatform;
